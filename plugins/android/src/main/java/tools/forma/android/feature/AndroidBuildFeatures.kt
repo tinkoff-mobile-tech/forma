2227097c230a3b5a8d4a185085fd547863540402
rma <rommaks91@yandex.ru>
2023-04-13 10:30:43 +0300

    add base class for build features container

--- plugins/android/src/main/java/tools/forma/android/feature/AndroidBuildFeatures.kt
+++ plugins/android/src/main/java/tools/forma/android/feature/AndroidBuildFeatures.kt
@@ -1,8 +1,6 @@
 package tools.forma.android.feature
 
-class ApplicationBuildFeaturesContainer(
-    val dataBinding: Boolean? = null,
-    val mlModelBinding: Boolean? = null,
+open class BaseBuildFeaturesContainer(
     val aidl: Boolean? = null,
     val buildConfig: Boolean? = null,
     val compose: Boolean? = null,
@@ -13,17 +11,48 @@ class ApplicationBuildFeaturesContainer(
     val viewBinding: Boolean? = null,
 )
 
+class ApplicationBuildFeaturesContainer(
+    val dataBinding: Boolean? = null,
+    val mlModelBinding: Boolean? = null,
+    aidl: Boolean? = null,
+    buildConfig: Boolean? = null,
+    compose: Boolean? = null,
+    prefab: Boolean? = null,
+    renderScript: Boolean? = null,
+    resValues: Boolean? = null,
+    shaders: Boolean? = null,
+    viewBinding: Boolean? = null,
+) : BaseBuildFeaturesContainer(
+    aidl = aidl,
+    buildConfig = buildConfig,
+    compose = compose,
+    prefab = prefab,
+    renderScript = renderScript,
+    resValues = resValues,
+    shaders = shaders,
+    viewBinding = viewBinding,
+)
+
 class LibraryBuildFeaturesContainer(
-    val aidl: Boolean? = null,
-    val buildConfig: Boolean? = null,
-    val compose: Boolean? = null,
-    val prefab: Boolean? = null,
-    val renderScript: Boolean? = null,
-    val resValues: Boolean? = null,
-    val shaders: Boolean? = null,
-    val viewBinding: Boolean? = null,
     val androidResources: Boolean? = null,
     val dataBinding: Boolean? = null,
     val mlModelBinding: Boolean? = null,
     val prefabPublishing: Boolean? = null,
+    aidl: Boolean? = null,
+    buildConfig: Boolean? = null,
+    compose: Boolean? = null,
+    prefab: Boolean? = null,
+    renderScript: Boolean? = null,
+    resValues: Boolean? = null,
+    shaders: Boolean? = null,
+    viewBinding: Boolean? = null,
+) : BaseBuildFeaturesContainer(
+    aidl = aidl,
+    buildConfig = buildConfig,
+    compose = compose,
+    prefab = prefab,
+    renderScript = renderScript,
+    resValues = resValues,
+    shaders = shaders,
+    viewBinding = viewBinding,
 )
